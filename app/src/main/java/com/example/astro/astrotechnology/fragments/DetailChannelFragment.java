d0cebe59232918e7e16809c9a86040a5ea6031f9
huynhcongminh12 <huynhcongminh12@gmail.com>
2017-11-10 18:39:36 +0700

    get datetime

--- app/src/main/java/com/example/astro/astrotechnology/fragments/DetailChannelFragment.java
+++ app/src/main/java/com/example/astro/astrotechnology/fragments/DetailChannelFragment.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 
 import com.example.astro.astrotechnology.R;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -19,13 +20,20 @@ import java.util.Date;
 
 public class DetailChannelFragment extends Fragment {
 
-
+    SimpleDateFormat DesiredFormat = new SimpleDateFormat("MM/dd/yyyy HH:MM:SS a");
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_detail_channel, container , false );
         Date currentTime = Calendar.getInstance().getTime();
-        Log.d("real time: " , "values: " + currentTime);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(currentTime);
+        calendar.add(Calendar.DAY_OF_YEAR, -7);
+        Date newDate = calendar.getTime();
+        String formattedDate = DesiredFormat.format(newDate.getTime());
+
+        Log.d("real time: " , "values: " + formattedDate);
         return view;
     }
 }
